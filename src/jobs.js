4a44566169424f9f61a6e4767be200a2f186b5c3
river2056 <chen0625tung@gmail.com>
2021-05-11 00:10:35 +0800

    auto update snapshots according to config and markets.json

--- src/jobs.js
+++ src/jobs.js
@@ -4,6 +4,7 @@ const config = require('../config/jobConfig.json');
 const eventEmitter = require('./appEvents');
 const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler');
 const { jobOptions } = require('./helper');
+const markets = require('../markets.json');
 
 const scheduler = new ToadScheduler();
 
@@ -11,7 +12,15 @@ const clearServerFiles = new Task('clear-server-files', () => {
     eventEmitter.emit('clear-files');
 });
 
+const updateSnapShots = new Task('update-snapshots', () => {
+    markets.forEach(market => {
+        eventEmitter.emit('fetch-stock', market);
+    });
+});
+
 const clearJob = new SimpleIntervalJob(jobOptions(config), clearServerFiles);
+const updateJob = new SimpleIntervalJob(jobOptions(config), updateSnapShots);
 scheduler.addSimpleIntervalJob(clearJob);
+scheduler.addSimpleIntervalJob(updateJob);
 
 module.exports = scheduler;
